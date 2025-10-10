a4f750c8abbf26ab0912ec82d008cd17dc7f07d4
nicolly54 <nicolly.valeski@escola.pr.gov.br>
2025-10-10 08:02:22 -0300

    ...

---
+++ penguntas.js
@@ -0,0 +1,15 @@
+criaCatao(
+    'programação'
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCatao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da França é Paris'
+)
+criaCatao(
+    'Programação',
+    'O que é função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
