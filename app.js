f41a74c1044f25e04eb7099bacb13dae153adc9f
nicolly54 <nicolly.valeski@escola.pr.gov.br>
2025-10-22 08:51:31 -0300

    ...

---
+++ app.js
@@ -0,0 +1,5 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementByld('conteiner')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
